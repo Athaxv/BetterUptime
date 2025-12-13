198e86317813d6e49ad020a30c772a123b2918bf
AtharvGaur <atharava112@gmail.com>
2025-12-13 13:47:42 +0530

    feat(redis): initialize Redis package with client connection and basic event handling

---
+++ packages/redis/src/index.ts
@@ -0,0 +1,23 @@
+import { createClient } from "redis";
+
+const client = createClient({
+    url: "redis://localhost:6379"
+});
+client.on?.("error", (err: Error) => console.log("Redis Client Error", err));
+await client.connect();
+console.log("Redis Client Connected");
+
+type websiteEvent = { url: String, id: String };
+
+export async function xAdd({ url, id }: websiteEvent) {
+    await client.xAdd('betteruptime:website', '*', {
+        url,
+        id
+    })
+}
+
+export async function BulkAdd(websites: websiteEvent[]) {
+    for (const website of websites) {
+        await xAdd(website);
+    }
+}
