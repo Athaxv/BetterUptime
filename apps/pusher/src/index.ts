ce6bb69feef6b8794ca282093202888f763d1f4c
AtharvGaur <atharava112@gmail.com>
2025-12-13 21:21:08 +0530

    feat(pusher): add initial pusher package with TypeScript configuration, package.json, and main entry point

---
+++ apps/pusher/src/index.ts
@@ -0,0 +1,18 @@
+import { db as prisma } from "@repo/db";
+import { BulkAdd } from "redis-stream/client";
+
+async function main(){
+    const websites  = await prisma.website.findMany({
+        select: {
+            url: true,
+            id: true
+        }
+    })
+
+    await BulkAdd(websites.map((w: { url: string; id: string }) => ({
+        url: w.url,
+        id: w.id
+    })));
+}
+
+main();
