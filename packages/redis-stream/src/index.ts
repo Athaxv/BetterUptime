4e2f526de88e6f57a885cabd1cc2254e4a69075e
AtharvGaur <atharava112@gmail.com>
2026-01-05 18:53:54 +0530

    feat(redis-stream): add xReadGroup and xAckBulk functions for message processing and acknowledgment in Redis stream

--- packages/redis-stream/src/index.ts
+++ packages/redis-stream/src/index.ts
@@ -8,6 +8,7 @@ await client.connect();
 console.log("Redis Client Connected");
 
 type websiteEvent = { url: string, id: string };
+type MessageType = { id: string, message: { url: string, id: string }};
 
 export async function xAdd({ url, id }: websiteEvent) {
     await client.xAdd('betteruptime:website', '*', {
@@ -23,4 +24,26 @@ export async function BulkAdd(websites: websiteEvent[]) {
             id: website.id
         });
     }
+}
+
+export async function xReadGroup(consumerGrp: string, workerId: string): Promise<MessageType[]> {
+    const res = await client.xReadGroup(consumerGrp, workerId, {
+        key: 'betteruptime:website',
+        id: '>',
+    },{
+        'COUNT': 5
+    })
+    // @ts-ignore
+    let messages: MessageType[] = res[0]?.messages as MessageType[];
+    // @ts-ignore
+    console.log("XReadGroup Response", res[0].messages);
+    return messages;
+}
+
+async function xAck(consumerGrp: string, streamId: string) {
+    await client.xAck("betteruptime:website", consumerGrp, streamId);
+}
+
+export async function xAckBulk(consumerGrp: string, streamIds: string[]) {
+    streamIds.map(streamId => xAck(consumerGrp, streamId));
 }
